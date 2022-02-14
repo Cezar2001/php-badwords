b339940ff9184ea062e77bcbf418d99909901728
Cezar <cezarlantosgmail.com>
2022-02-14 16:15:06 +0100

    finish

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$paragraph = "Il mio primo paragrafo in php";
+$lengthParagraph = strlen($paragraph);
+$badWord = $_GET["primo"];
+$newParagraph = str_replace($badWord, "***", $paragraph);
+$lengthNewParagraph = strlen($newParagraph);
+var_dump($badWord);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1><?php echo $paragraph ?></h1>
+    <h2>La lunghezza di paragrafo è di <?php echo $lengthParagraph ?> caratteri.</h2>
+    <h1><?php echo $newParagraph ?></h1>
+    <h2>La lunghezza di questo paragrafo censurato è di <?php echo $lengthNewParagraph ?> caratteri.</h2>
+
+</body>
+</html>
